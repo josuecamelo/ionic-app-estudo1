49c63ee6de66bc08d4d8d090141a2b50067147cb
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2018-04-12 12:16:36 -0300

    Obtendo Token

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -14,12 +9,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
+  user = {
+    email: 'user@user.com',
+    password: ''
+  };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: HttpClient) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  login(){
+    this.http.post('http://localhost:8000/api/login', this.user)
+        .subscribe(data => console.log(data));
   }
 
 }
